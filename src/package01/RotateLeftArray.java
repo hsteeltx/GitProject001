ddd3ccc109c682cbe4877188feead8177832892e
hsteeltx <79743650+hsteeltx@users.noreply.github.com>
2021-03-13 14:29:29 -0600

    Update RotateLeftArray.java

--- src/package01/RotateLeftArray.java
+++ src/package01/RotateLeftArray.java
@@ -18,6 +18,7 @@ public class RotateLeftArray {
 		}
 
 		//this is just try
+		//this is the second try
 		//switching first and last element
 
 		//this will switch the places of first and second elements
